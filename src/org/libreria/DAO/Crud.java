6075633a7e6890ac42f9a12265cc621a2f4d1727
Juan Esteban <jinteriano-2023449@kinal.edu.gt>
2026-09-24 11:16:53 -0600

    feat(dao): crear interfaz Crud

---
+++ src/org/libreria/DAO/Crud.java
@@ -0,0 +1,17 @@
+package org.libreria.DAO;
+
+/**
+ *
+ * @author informatica
+ */
+import java.util.ArrayList;
+
+public interface Crud<T, K> {
+    boolean crear(T entidad);
+    boolean actualizar(T entidad);
+    boolean eliminar(K id);
+    T buscarPorId(K id);
+    ArrayList<T> listarTodos();
+}
+    
+
