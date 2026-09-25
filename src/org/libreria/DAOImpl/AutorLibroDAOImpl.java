6eabc76371604505d971f598b6c606c4a1cc924e
Juan Esteban <jinteriano-2023449@kinal.edu.gt>
2026-09-25 10:46:20 -0600

    docs(javadoc): documentar AutorLibroDAOImpl

--- src/org/libreria/DAOImpl/AutorLibroDAOImpl.java
+++ src/org/libreria/DAOImpl/AutorLibroDAOImpl.java
@@ -1,8 +1,11 @@
 package org.libreria.DAOImpl;
 
 /**
+ * Representa la separacion entre este codigo y su base de datos SQL
  *
- * @author informatica
+ * @author Esteban Interiano
+ * @version 1.0.0
+ * @see org.libreria.model.AutorLibro
  */
 import java.sql.CallableStatement;
 import java.sql.Connection;
@@ -14,8 +17,16 @@ import org.libreria.exception.DaoException;
 import org.libreria.model.AutorLibro;
 import org.libreria.util.Conexion;
 
+/**
+ *
+ * @author Esteban Interiano
+ */
 public class AutorLibroDAOImpl implements AutorLibroDAO {
 
+    /**
+     * obtiene una lista de los libros junto con sus autores, el id del autor y el isbn del libro
+     * @return devuelve una lista de todos los autores de libro
+     */
     @Override
     public ArrayList<AutorLibro> listarTodos() {
         ArrayList<AutorLibro> lista = new ArrayList<>();
@@ -36,6 +47,11 @@ public class AutorLibroDAOImpl implements AutorLibroDAO {
         return lista;
     }
 
+    /**
+     * obtiene el id del autor libro
+     * @param idAutorLibro identificador unico del autorlibro
+     * @return devuelve el id de autor libro
+     */
     @Override
     public AutorLibro buscarPorId(Integer idAutorLibro) {
         AutorLibro al = null;
@@ -57,6 +73,11 @@ public class AutorLibroDAOImpl implements AutorLibroDAO {
         return al;
     }
 
+    /**
+     * obtiene un valor verdadero o falso para poder crear un nuevo autor libro
+     * @param autorLibro persona que escribio el libro
+     * @return devuelve la validacion para la creacion de un nuevo autor libro
+     */
     @Override
     public boolean crear(AutorLibro autorLibro) {
         String sql = "{call sp_insertarautorlibro(?,?)}";
@@ -70,6 +91,11 @@ public class AutorLibroDAOImpl implements AutorLibroDAO {
         }
     }
 
+    /**
+     * obtiene un valor verdadero o falso para poder hacer una actualizacion de un autorlibro
+     * @param autorLibro persona que escribio el libro
+     * @return devuelve la validacion para el proceso de actualizacion de autorlibro
+     */
     @Override
     public boolean actualizar(AutorLibro autorLibro) {
         String sql = "{call sp_actualizarautorlibro(?,?,?)}";
@@ -84,6 +110,11 @@ public class AutorLibroDAOImpl implements AutorLibroDAO {
         }
     }
 
+    /**
+     *  obtiene un valor verdadero o falso para poder proceder con la eliminacion de un autor libro por medio de su id
+     * @param idAutorLibro identificador del autorlibro
+     * @return devuelve la validacion para poder eliminar un autorlibro 
+     */
     @Override
     public boolean eliminar(Integer idAutorLibro) {
         String sql = "{call sp_eliminarautorlibro(?)}";
