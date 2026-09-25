90807e9a184de8372c7a8d1e5c05c1d7475f4e4d
Juan Esteban <jinteriano-2023449@kinal.edu.gt>
2026-09-25 09:06:56 -0600

    feat(dao): implementar VentaDAO

---
+++ src/org/libreria/DAO/VentaDAO.java
@@ -0,0 +1,16 @@
+
+package org.libreria.DAO;
+
+/**
+ *
+ * @author informatica
+ */
+import java.util.List;
+import org.libreria.model.LineaVenta;
+import org.libreria.model.Venta;
+
+public interface VentaDAO extends Crud<Venta, Integer>{
+    //crearVenta inserta el encabezado de la venta, sus líneas y descuenta el stock.
+    //Devuelve el no_venta generado (o -1 si falla).
+    int crearVenta(Venta venta, List<LineaVenta> lineas);
+}
