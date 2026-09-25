0137f486ba61e26658bd18b69db18133dc190f2d
Juan Esteban <jinteriano-2023449@kinal.edu.gt>
2026-09-25 08:12:45 -0600

    feat(dao): implementar AutorLibroDAO

---
+++ src/org/libreria/DAO/AutorLibroDAO.java
@@ -0,0 +1,11 @@
+package org.libreria.DAO;
+
+/**
+ *
+ * @author informatica
+ */
+import org.libreria.model.AutorLibro;
+
+public interface AutorLibroDAO extends Crud<AutorLibro, Integer>{
+    
+}
