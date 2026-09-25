ba5cff2e95ec04ef164b980bc33efb425a0653a4
Juan Esteban <jinteriano-2023449@kinal.edu.gt>
2026-09-25 08:28:59 -0600

    feat(dao) Implementar DetalleVentaDAO

---
+++ src/org/libreria/DAO/DetalleVentaDAO.java
@@ -0,0 +1,11 @@
+package org.libreria.DAO;
+
+/**
+ *
+ * @author informatica
+ */
+import org.libreria.model.DetalleVenta;
+
+public interface DetalleVentaDAO extends Crud<DetalleVenta, Integer>{
+
+}
