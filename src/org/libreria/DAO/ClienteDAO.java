639f7c8c4b1d1ebe685005a8cad194a981552bcb
Juan Esteban <jinteriano-2023449@kinal.edu.gt>
2026-09-25 08:26:06 -0600

    docs(javadoc): documentar ClienteDAO

--- src/org/libreria/DAO/ClienteDAO.java
+++ src/org/libreria/DAO/ClienteDAO.java
@@ -1,14 +1,17 @@
 package org.libreria.DAO;
 
 /**
+ * Representa separacion de los datos del codigo con la base de datos de SQL para Cliente
  *
- * @author informatica
+ * @author Esteban Interiano
+ * @version 1.0.0
+ * @see org.libreria.DAO.ClienteDAO
  */
 import org.libreria.model.Cliente;
 
 /**
  *
- * @author aruba
+ * @author Esteban Interiano
  */
 public interface ClienteDAO extends Crud<Cliente, Long>{
     
