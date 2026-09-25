8fa85fbc2070c67951cf479dfaeba4ccee712713
Juan Esteban <jinteriano-2023449@kinal.edu.gt>
2026-09-25 08:35:40 -0600

    feat(dao): Implementar EditorialDAO

---
+++ src/org/libreria/DAO/EditorialDAO.java
@@ -0,0 +1,15 @@
+package org.libreria.DAO;
+
+/**
+ *
+ * @author informatica
+ */
+import org.libreria.model.Editorial;
+
+/**
+ *
+ * @author aruba
+ */
+public interface EditorialDAO extends Crud<Editorial, String>{
+    
+}
