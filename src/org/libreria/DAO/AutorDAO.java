a590145da590c57d6a45482beedaa209cb12fc16
Juan Esteban <jinteriano-2023449@kinal.edu.gt>
2026-09-25 08:08:36 -0600

    docs(javadoc): codumentar AutorDAO

--- src/org/libreria/DAO/AutorDAO.java
+++ src/org/libreria/DAO/AutorDAO.java
@@ -1,12 +1,18 @@
 package org.libreria.DAO;
 
 /**
+ * Representa la separacion de este codigo con la base de datos SQL en la clase de Autor
  *
- * @author informatica
+ * @author Esteban Interiano
+ * @version 1.0.0
+ * @see org.libreria.model.AutorLibro
  */
 import org.libreria.model.Autor;
 
-
+/**
+ * r
+ * @author Esteban Interiano
+ */
 public interface AutorDAO extends Crud<Autor, Integer>{
     
 }
