73e7a08e716fa2750c46c1e2634d1e0840b9ce5e
Juan Esteban <jinteriano-2023449@kinal.edu.gt>
2026-09-25 08:18:22 -0600

    feat(dao): implementar CategoriaDAO

---
+++ src/org/libreria/DAO/CategoriaDAO.java
@@ -0,0 +1,15 @@
+package org.libreria.DAO;
+
+/**
+ *
+ * @author informatica
+ */
+import org.libreria.model.Categoria;
+
+/**
+ *
+ * @author aruba
+ */
+public interface CategoriaDAO extends Crud<Categoria, Integer>{
+    
+}
