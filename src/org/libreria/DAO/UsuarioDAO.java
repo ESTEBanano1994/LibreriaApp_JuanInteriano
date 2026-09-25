dcce6620e3af9e96528794d370d5df84f29685bd
Juan Esteban <jinteriano-2023449@kinal.edu.gt>
2026-09-25 08:51:14 -0600

    feat(dao): Implementar UsuarioDAO

---
+++ src/org/libreria/DAO/UsuarioDAO.java
@@ -0,0 +1,24 @@
+
+package org.libreria.DAO;
+
+/**
+ *
+ * @author informatica
+ */
+import java.util.ArrayList;
+import org.libreria.model.Usuario;
+
+/**
+ *
+ * @author aruba
+ */
+public interface UsuarioDAO {
+    public Usuario iniciarSesion(String usernarme, String passwordHash);
+    public boolean crearUsuario(Usuario usuario);
+    public boolean actualizarUsuario(Usuario usuario);
+    public boolean cambiarPassword(int idUsuario, String passwordHash);
+    public boolean desactivarUsuario(int idUsuario);
+    public boolean eliminarUsuario(int idUsuario);
+    public ArrayList<Usuario> listarTodosUsuarios();
+    public Usuario obtenerUsuarioPorId(int idUsuario);
+}
