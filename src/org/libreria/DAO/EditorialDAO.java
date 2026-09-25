9568d4bec9ca0733ad6b585eee80c4e3bb0a468f
Juan Esteban <jinteriano-2023449@kinal.edu.gt>
2026-09-25 08:39:38 -0600

    docs(javadoc): Documentar EditorialDAO

--- src/org/libreria/DAO/EditorialDAO.java
+++ src/org/libreria/DAO/EditorialDAO.java
@@ -1,14 +1,17 @@
 package org.libreria.DAO;
 
 /**
+ * Representa la relación entre un autor y un libro dentro del sistema.
  *
- * @author informatica
+ * @author Esteban Interiano
+ * @version 1.0.0
+ * @see org.libreria.model.AutorLibro
  */
 import org.libreria.model.Editorial;
 
 /**
  *
- * @author aruba
+ * @author Esteban Interiano
  */
 public interface EditorialDAO extends Crud<Editorial, String>{
     
