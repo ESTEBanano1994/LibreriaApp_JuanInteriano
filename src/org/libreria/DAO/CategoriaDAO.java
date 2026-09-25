1cbd7f38e951297d518e6a2dcf3e8193c2a7c1e7
Juan Esteban <jinteriano-2023449@kinal.edu.gt>
2026-09-25 08:21:51 -0600

    docs(javadoc): documentar CategoriaDAO

--- src/org/libreria/DAO/CategoriaDAO.java
+++ src/org/libreria/DAO/CategoriaDAO.java
@@ -1,14 +1,17 @@
 package org.libreria.DAO;
 
 /**
+ * Representa la relación entre un autor y un libro dentro del sistema.
  *
- * @author informatica
+ * @author Esteban Interiano
+ * @version 1.0.0
+ * @see org.libreria.DAO.CategoriaDAO
  */
 import org.libreria.model.Categoria;
 
 /**
  *
- * @author aruba
+ * @author Esteban Interiano
  */
 public interface CategoriaDAO extends Crud<Categoria, Integer>{
     
